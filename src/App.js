6cfddfc528c1ef90745ed27c7bd19350630bfd0f
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2023-03-27 15:30:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <p>Hora de apreder!</p>
+    </div>
+  );
+}
+
+export default App;
+
+
